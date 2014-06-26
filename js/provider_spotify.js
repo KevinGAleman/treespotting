5c6b46c12dc9efe48d82d71a0e2efdd3fd9c6c63
KevinGAleman <kevin.aleman@utexas.edu>
2014-06-26 13:25:22 -0700

    Created a getArtistTopTracks function
    Also commented some code.

--- js/provider_spotify.js
+++ js/provider_spotify.js
@@ -20,10 +20,30 @@ Spotify.getArtistImgFromId = function(spotifyId, callback) {
 	}
 };
 
+/**
+* Takes a Spotify artist name and returns the img URL and spotifyId
+* @param artistName The name of the artist
+* @param callback The callback function to call with the img url and spotifyId
+*/
 Spotify.getArtistImgFromName = function(artistName, callback) {
 	var spotApiUrl = "https://api.spotify.com/v1/search?q=" + artistName + "&type=artist";
 	
 	$.getJSON(spotApiUrl, function(json) {
 		callback(json.artists.items[0].images[1].url, json.artists.items[0].uri);
+	});
+};
+
+/**
+* Takes a Spotify artist ID and returns the top tracks for the artist
+* @param spotifyId The Spotify ID of the artist, prefixed by spotify:artist:
+* @param callback The callback function to call with the JSON object full of track objects
+*/
+Spotify.getArtistTopTracks = function(spotifyId, callback) {
+	var countryCode = "US"; //maybe adapt this to localized version later
+	var id = spotifyId.replace("spotify:artist:", "");
+	var spotApiUrl = "https://api.spotify.com/v1/artists/" + id + "/top-tracks?country=" + countryCode;
+	
+	$.getJSON(spotApiUrl, function(json) {
+		callback(json);
 	});
 };
